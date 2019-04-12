f9f8bb3c9448a6074aeae66b0dbea3192c8ec3da
Shreyas Agnihotri <shreyas.v.agnihotri@gmail.com>
2019-04-12 14:59:51 -0400

    Completed startpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import './style.scss';
+import $ from 'jquery';
+
+let num = 0;
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds`);
+  num += 1;
+}, 1000);
